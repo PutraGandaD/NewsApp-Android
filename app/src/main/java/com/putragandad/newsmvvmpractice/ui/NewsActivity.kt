8facb2b259d7877cb49c923b4ed14fa489de3bf4
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-04-26 02:01:26 +0700

    Successfully setup Bottom Navigation with Navigation Component

---
+++ app/src/main/java/com/putragandad/newsmvvmpractice/ui/NewsActivity.kt
@@ -0,0 +1,23 @@
+package com.putragandad.newsmvvmpractice.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
+import com.putragandad.newsmvvmpractice.R
+import com.putragandad.newsmvvmpractice.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavController = this.findNavController(R.id.newsNavHostFragment)
+
+        binding.bottomNavigation.setupWithNavController(bottomNavController)
+    }
+}
